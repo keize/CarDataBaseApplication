40047eb952600dd10a2e796ea12e9c58f15bd9fe
Kate Moroz <kattmoroz@gmail.com>
2020-11-13 00:12:08 +0200

    without navigation

--- app/src/main/java/com/example/carsdatabaseapi/SecondActivity.kt
+++ app/src/main/java/com/example/carsdatabaseapi/SecondActivity.kt
@@ -3,9 +3,9 @@ package com.example.carsdatabaseapi
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class ItemActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_item)
+        setContentView(R.layout.activity_second)
     }
 }
